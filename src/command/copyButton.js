bf47464f98e7ccb9af4cd7af44a8cf955efde7cc
Willyams Yujra <willyams.yujra@argano.com>
2026-01-16 17:15:06 -0400

    + manifest2

--- src/command/copyButton.js
+++ src/command/copyButton.js
@@ -1,18 +1,25 @@
 import { cleanText } from "./util.js";
 
-browser.runtime.onInstalled.addListener(() => {
-  browser.contextMenus.create({
+const browserAPI = window.browser || window.chrome;
+
+browserAPI.runtime.onInstalled.addListener(() => {
+  browserAPI.contextMenus.create({
     id: "copy-clean-unicode",
     title: "Copy Clean Text",
     contexts: ["selection"],
   });
 });
 
-browser.contextMenus.onClicked.addListener(async (info, tab) => {
+browserAPI.contextMenus.onClicked.addListener(async (info, tab) => {
   const text = info.selectionText;
   if (!text) return;
   if (info.menuItemId === "copy-clean-unicode") {
     const cleaned = cleanText(text);
-    await navigator.clipboard.writeText(cleaned);
+    try {
+      await navigator.clipboard.writeText(cleaned);
+      console.log("Texto limpio copiado:", cleaned);
+    } catch (err) {
+      console.error("No se pudo copiar el texto:", err);
+    }
   }
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig, type PluginOption } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import webExtension from '@samrum/vite-plugin-web-extension';
-
+//@ts-ignore
+import autoZip from 'vite-plugin-auto-zip';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,18 +10,16 @@ export default defineConfig({
   server: {
     cors: true,
   },
-  resolve: {
-    alias: {
-      '@vite/client.js': '@vite/client'
-    }
-  },
   plugins: [
     react(),
+    autoZip('cct-chrome.zip', './dist', './release'),
+    autoZip('cct-firefox.zip', './dist', './release'),
     webExtension(
       {
         manifest: {
           manifest_version: 2,
           name: "Copy Clean Text",
+
           version: "1.0.0",
           permissions: [
             "storage",
@@ -33,9 +32,11 @@ export default defineConfig({
           },
           icons: {
             "16": "icons/favicon-16x16.png",
-            "32": "icons/favicon-32x32.png"
+            "32": "icons/favicon-32x32.png",
+            "48": "icons/favicon-48x48.png",
+            "128": "icons/favicon-128x128.png"
           }
         }
-      }) as PluginOption,
+      }) as PluginOption
   ],
 })
